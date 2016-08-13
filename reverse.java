9ad15fb46762cd212106478a2f4bf8aa32a4008d
pappathibarjana <pappathi.barjana379@gmail.com>
2016-08-13 12:00:11 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -4,7 +4,7 @@ class reverse
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-String s=sc.nextLine();
+String s=sc.next();
 StringBuffer sb=new StringBuffer(s);
 System.out.println(sb.reverse().toString());
 }
