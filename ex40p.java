f165767e2669d52c971a84bd0bde07894835cd98
pappathibarjana <pappathi.barjana379@gmail.com>
2016-09-05 08:44:47 -0700

    Add files via upload

---
+++ ex40p.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class ex40p
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+n=Math.abs(n);
+int s=0;
+if(n==0)
+System.out.println("palindrome");
+else
+{
+while(n!=0)
+{
+s+=n%10;
+n=n/10;
+}
+StringBuffer sb=new StringBuffer();
+sb.append(s);
+if(sb.charAt(0)=='0')
+{
+System.out.println("not a palindrome");
+}
+else
+{
+if(sb.toString().equals(sb.reverse().toString()))
+System.out.println("palindrome");
+else
+System.out.println("not a palindrome");
+}
+}
+}
+}
